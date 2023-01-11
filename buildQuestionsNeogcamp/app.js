01545c54a191ce253ad727d50cb869667dee6788
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-12 00:37:39 +0530

    Prime Number check

--- buildQuestionsNeogcamp/app.js
+++ buildQuestionsNeogcamp/app.js
@@ -12,3 +12,23 @@ if (userInput >= 0) {
 } else {
   console.log("Please enter positive number\n");
 }
+
+//program to check a number is prime or not
+var readlineSync = require("readline-sync");
+let userInput = readlineSync.question("enter a number\n");
+
+function checkPrime(number) {
+  let flag = 0;
+  for (let i = 2; i < number / 2; i++) {
+    if (number % i === 0) {
+      flag = 1;
+    }
+  }
+  if (flag === 1) {
+    console.log("Not Prime Number!");
+  } else {
+    console.log("Prime Number!");
+  }
+}
+
+checkPrime(userInput);
