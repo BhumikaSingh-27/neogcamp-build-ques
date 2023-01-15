c57f2fd426b3945660a200529cc37fc7ecb65b6e
BhumikaSingh-27 <100286699+BhumikaSingh-27@users.noreply.github.com>
2023-01-15 23:41:40 +0530

    adv func

--- buildQuestionsNeogcamp/AdvancedFunc.js
+++ buildQuestionsNeogcamp/AdvancedFunc.js
@@ -3,64 +3,65 @@
 //charCodeAt() --convert to ascii
 
 function encodeString(str, n) {
-    let secretCode = ""
+  let secretCode = "";
   for (let i = 0; i < str.length; i++) {
-
     var code = str.charCodeAt(i) + n;
     // console.log(code)
-    secretCode += String.fromCharCode(code)
+    secretCode += String.fromCharCode(code);
   }
-console.log(secretCode)
+  console.log(secretCode);
 }
 
-encodeString("neogcamp", 2)
-
+encodeString("neogcamp", 2);
 
 //Given a sentence, return a sentence with first letter of all words as capital.
 
-
 function toSentenceCase(sentence) {
-    let words = sentence.split(" ");
-    let newArr = [];
-    // console.log(words)
-    words.map(word => {
-      let arr = word[0].toUpperCase()
-      for (let i = 1; i < word.length; i++) {
-        arr += word[i];
-      }
-      newArr.push(arr)
-    })
-    return newArr.join(" ")
-  }
-  
-  const finalSentence = toSentenceCase('we are neoGrammers')
-  console.log(finalSentence)
+  let words = sentence.split(" ");
+  let newArr = [];
+  // console.log(words)
+  words.map((word) => {
+    let arr = word[0].toUpperCase();
+    for (let i = 1; i < word.length; i++) {
+      arr += word[i];
+    }
+    newArr.push(arr);
+  });
+  return newArr.join(" ");
+}
 
+const finalSentence = toSentenceCase("we are neoGrammers");
+console.log(finalSentence);
 
 //Given an array of numbers, your function should return an array in the ascending order.
- 
-const sortedArray = function(arr){
-    let len = arr.length;
-    let i = 0;
-    let j = 1;
-    while(j<len){
-        if(arr[i]>arr[j]){
-            let swap = arr[j];
-            arr[j] = arr[i]
-            arr[i] = swap;
-            i++;
-            j++;
-        }
-    }
-    return arr;
-}
 
-console.log(sortedArray([100,83,32,9,45,61]))
+const sortedArray = function (arr) {
+  let len = arr.length;
+  let i = 0;
+  let j = 1;
+  while (j < len) {
+    if (arr[i] > arr[j]) {
+      let swap = arr[j];
+      arr[j] = arr[i];
+      arr[i] = swap;
+      i++;
+      j++;
+    }
+  }
+  return arr;
+};
+
+console.log(sortedArray([100, 83, 32, 9, 45, 61]));
 
 //Given a sentence, your function should reverse the order of characters in each word, keeping same sequence of words.
-function reverseCharactersOfWord(sentence){
-
-    return [...sentence].reduce((x,y)=>y.concat(x))
+function reverseCharactersOfWord(sentence) {
+  // return [...sentleence].reduce((x,y)=>y.concat(x))
+  let reversedArray = [];
+  let arrayOfWords = sentence.split(" ");
+  for (let i = 0; i < arrayOfWords.length; i++) {
+    reversedArray.push(arrayOfWords[i].split("").reverse().join(""));
+  }
+  return reversedArray;
 }
 
-reverseCharactersOfWord("we are")
+console.log(reverseCharactersOfWord("we are"));
